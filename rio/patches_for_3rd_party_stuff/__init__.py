519d36efd8d8c9927a324ec3b0a30f49158a1565
Aran-Fey <pinterits.paul@gmail.com>
2024-04-13 21:39:33 +0200

    fix crash on linux

--- rio/patches_for_3rd_party_stuff/__init__.py
+++ rio/patches_for_3rd_party_stuff/__init__.py
@@ -1,6 +1,10 @@
-from . import (
-    IocpProactor_accept_locals_accept_coro as IocpProactor_accept_locals_accept_coro,
-)
-from . import (
-    ProactorBasePipeTransport_call_connection_lost as ProactorBasePipeTransport_call_connection_lost,
-)
+import sys
+
+if sys.platform == "win32":
+    from . import (
+        IocpProactor_accept_locals_accept_coro,
+        ProactorBasePipeTransport_call_connection_lost,
+    )
+
+    del IocpProactor_accept_locals_accept_coro
+    del ProactorBasePipeTransport_call_connection_lost
