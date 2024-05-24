9fd2067b386577ceb69bd7c30ff17556f43dd9c4
Aran-Fey <pinterits.paul@gmail.com>
2024-05-24 11:23:22 +0200

    update publishing script

--- scripts/publish_new_release.py
+++ scripts/publish_new_release.py
@@ -1,26 +1,39 @@
 import subprocess
 import sys
 
-import requests
 import revel
 
-import rio
+
+DEV_BRANCH = "dev"
+RELEASE_BRANCH = "main"
 
 
 def main() -> None:
     revel.print("Running sanity checks...")
 
-    # Make sure we're on the correct branch
+    ensure_branch(DEV_BRANCH)
+    ensure_no_uncommitted_changes()
+    ensure_up_to_date_with_remote()
+
+    build_frontend()
+    ensure_tests_pass()
+
+    revel.print("Everything is in order.")
+    make_new_release()
+
+
+def ensure_branch(branch_name: str) -> None:
     process = subprocess.run(
         ["git", "branch", "--show-current"],
         check=True,
         capture_output=True,
         text=True,
     )
-    if process.stdout.strip("\n") != "dev":
-        revel.fatal("You must checkout the 'dev' branch")
+    if process.stdout.strip("\n") != branch_name:
+        revel.fatal(f'You must checkout the "{branch_name}" branch')
 
-    # Make sure there are no uncommitted changes
+
+def ensure_no_uncommitted_changes() -> None:
     process = subprocess.run(
         ["git", "status", "--porcelain", "--untracked-files=no"],
         check=True,
@@ -30,45 +43,56 @@ def main() -> None:
     if process.stdout.strip():
         revel.fatal("There are uncommitted changes")
 
-    # Make sure the version number got bumped
-    if rio.__version__ == get_latest_published_version():
-        revel.fatal("You forgot to increment the version number")
 
-    # Build the TS code
-    #
+def ensure_up_to_date_with_remote() -> None:
+    subprocess.run(["git", "remote", "update"], check=True)
+    process = subprocess.run(
+        ["git", "status", "--porcelain", "--untracked-files=no"],
+        check=True,
+        capture_output=True,
+        text=True,
+    )
+    if process.stdout.strip():
+        revel.fatal("Local branch is not up-to-date with remote")
+
+
+def build_frontend() -> None:
     # Note: `shell=True` is required on Windows because `npm` is a `.cmd` file
     # and not a `.exe`
     subprocess.run(["npm", "run", "build"], shell=True, check=True)
 
-    # Run the test suite
-    if tests_pass():
-        revel.print("Everything is in order. Publishing...")
-    else:
-        # revel.fatal("The test don't pass")
-        if not revel.select_yes_no(
-            "The test suite does not pass. Do you want to publish anyway?"
-        ):
-            sys.exit(1)
 
-        revel.print("Publishing...")
+def ensure_tests_pass() -> None:
+    process = subprocess.run(["rye", "test", "--", "-x", "--disable-warnings"])
+    if process.returncode != 0:
+        revel.fatal("The test suite does not pass")
 
-    # Merge dev into main and push
-    subprocess.run(["git", "fetch", ".", "dev:main"], check=True)
-    subprocess.run(["git", "push", "-u", "origin", "main"], check=True)
+
+def make_new_release() -> None:
+    # Bump the version
+    version_part_to_bump = revel.select(
+        {
+            "bugfix": "micro",
+            "minor": "minor",
+            "major": "major",
+        },
+        prompt="What kind of release is this?",
+    )
+    subprocess.run(
+        [sys.executable, "-m", "hatch", "version", version_part_to_bump],
+        check=True,
+    )
+    subprocess.run(["git", "commit", "--all", "-m", "bump version"], check=True)
+
+    # Merge DEV_BRANCH into RELEASE_BRANCH and push
+    subprocess.run(
+        ["git", "fetch", ".", f"{DEV_BRANCH}:{RELEASE_BRANCH}"], check=True
+    )
+    subprocess.run(["git", "push", "-u", "origin", RELEASE_BRANCH], check=True)
 
     # Publish
     subprocess.run(["rye", "build", "--clean"], check=True)
     subprocess.run(["rye", "publish"], check=True)
-
-
-def get_latest_published_version() -> str:
-    response = requests.get(f"https://pypi.org/pypi/rio-ui/json")
-    return response.json()["info"]["version"]
-
-
-def tests_pass() -> bool:
-    process = subprocess.run(["rye", "test", "--", "-x", "--disable-warnings"])
-    return process.returncode == 0
 
 
 if __name__ == "__main__":
