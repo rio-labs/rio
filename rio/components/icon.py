a54d6bc0465918f46b18bae5608e9363691491b6
Jakob Pinterits <jakob.pinterits@gmail.com>
2024-09-20 08:44:34 +0200

    improved icon docs

--- rio/components/icon.py
+++ rio/components/icon.py
@@ -89,9 +89,39 @@ class Icon(FundamentalComponent):
     @staticmethod
     def register_icon_set(
         set_name: str,
-        icon_set_archive_path: Path,
+        set_archive_path: Path,
     ) -> None:
-        icon_registry.register_icon_set(set_name, icon_set_archive_path)
+        """
+        Adds an icon set to the global registry. This allows the icons to be
+        accessed as `"icon_set/icon_name"` or `"icon_set/icon_name:variant"`.
+
+        There must not already be a set with the given name.
+
+        The icon set is a `.tar.xz` compressed archive and must contain exactly
+        one directory, which must be named identically to the icon set. Files
+        located in the root of that directory can be accessed as
+        `"icon_set/icon_name"`. Files located in a subdirectory can be accessed
+        as `"icon_set/icon_name:variant"`.
+
+        For SVG files to work as icons...
+
+        - They must have a `viewBox` attribute, but no height or width
+        - They must contain exactly one XML root node: `<svg>...</svg>`.
+          Comments also count!
+        - Rio colors paths by assigning a `fill` to the SVG root. This only
+          works as long as the SVG paths don't have a `<style>` assigned already.
+
+
+        ## Parameters
+
+        `set_name`: The name of the new icon set. This will be used to access
+            the icons.
+
+        `icon_set_archive_path`: The path to the `.tar.xz` archive containing the
+            icon set.
+        """
+
+        icon_registry.register_icon_set(set_name, set_archive_path)
 
     @staticmethod
     def register_single_icon(
@@ -100,7 +130,36 @@ class Icon(FundamentalComponent):
         icon_name: str,
         variant_name: str | None = None,
     ) -> None:
-        icon_registry.register_icon(
+        """
+        Adds a single icon to the global registry. This allows the icon to be
+        accessed as `"icon_set/icon_name"` or `"icon_set/icon_name:variant"`.
+
+        `icon_source` needs to be the path to a single SVG file. For SVG files
+        to work as icons, they need to meet some conventions:
+
+        - They must have a `viewBox` attribute, but no height or width
+        - They must contain exactly one XML root node: `<svg>...</svg>`.
+          Comments also count!
+        - Rio colors paths by assigning a `fill` to the SVG root. This only
+        works as long as the SVG paths don't have a `<style>` assigned
+        already.
+
+
+        ## Parameters
+
+        `icon_source`: The path to the SVG file containing the icon.
+
+        `set_name`: The name of the new icon set. This will be used to access
+            the icons.
+
+        `icon_name`: The name of the icon. This will be used to access the
+            icon.
+
+        `variant_name`: The name of the variant. This will be used to access
+            the icon. If not specified, the default variant will be used.
+        """
+
+        icon_registry.register_single_icon(
             icon_source, set_name, icon_name, variant_name
         )
 
--- rio/icon_registry.py
+++ rio/icon_registry.py
@@ -265,78 +265,28 @@ def all_icons_in_set(
 
 
 def register_icon_set(
-    icon_set_name: str,
-    icon_set_archive_path: Path,
+    set_name: str,
+    set_archive_path: Path,
 ) -> None:
     """
-    Add an icon set to the global registry. This allows the icons to be accessed
-    as `"icon_set/icon_name"` or `"icon_set/icon_name:variant"`.
-
-    There must not already be a set with the given name.
-
-    The icon set is a `.tar.xz` compressed archive and must contain exactly one
-    directory, which must be named identically to the icon set. Files located in
-    the root of that directory can be accessed as `"icon_set/icon_name"`. Files
-    located in a subdirectory can be accessed as `"icon_set/icon_name:variant"`.
-
-    For SVG files to work as icons...
-
-    - They must have a `viewBox` attribute, but no height or width
-    - They must contain exactly one XML root node: `<svg>...</svg>`. This also
-      goes for comments!
-    - Rio colors paths by assigning a `fill` to the SVG root. This only works as
-      long as the SVG paths don't have a `<style>` assigned already.
-
-
-    ## Parameters
-
-    `icon_set_name`: The name of the new icon set. This will be used to access
-        the icons.
-
-    `icon_set_archive_path`: The path to the `.tar.xz` archive containing the
-        icon set.
+    See the docs of `Icon.register_icon_set`.
     """
-    if icon_set_name in icon_set_archives:
+    if set_name in icon_set_archives:
         raise ValueError(
-            f"There is already an icon set named `{icon_set_name}`"
+            f"There is already an icon set named `{set_name}`"
         )
 
-    icon_set_archives[icon_set_name] = icon_set_archive_path
+    icon_set_archives[set_name] = set_archive_path
 
 
-def register_icon(
+def register_single_icon(
     icon_source: Path,
-    icon_set_name: str,
+    set_name: str,
     icon_name: str,
     variant_name: str | None = None,
 ) -> None:
     """
-    Add a single icon to the global registry. This allows the icon to be
-    accessed as `"icon_set/icon_name"` or `"icon_set/icon_name:variant"`.
-
-    `icon_source` needs to be the path to a single SVG file. For SVG files
-    to work as icons...
-
-    - They must have a `viewBox` attribute, but no height or width
-    - They must contain exactly one XML root node: `<svg>...</svg>`. This
-      also goes for comments!
-    - Rio colors paths by assigning a `fill` to the SVG root. This only
-      works as long as the SVG paths don't have a `<style>` assigned
-      already.
-
-
-    ## Parameters
-
-    `icon_source`: The path to the SVG file containing the icon.
-
-    `set_name`: The name of the new icon set. This will be used to access
-        the icons.
-
-    `icon_name`: The name of the icon. This will be used to access the
-        icon.
-
-    `variant_name`: The name of the variant. This will be used to access
-        the icon. If not specified, the default variant will be used.
+    See the docs of `Icon.register_single_icon`.
     """
 
     # Try to load the icon
@@ -344,8 +294,8 @@ def register_icon(
 
     # Add it to the icon registry's cache
     if variant_name is None:
-        name = f"{icon_set_name}/{icon_name}"
+        name = f"{set_name}/{icon_name}"
     else:
-        name = f"{icon_set_name}/{icon_name}:{variant_name}"
+        name = f"{set_name}/{icon_name}:{variant_name}"
 
     cached_icons[name] = svg_source
