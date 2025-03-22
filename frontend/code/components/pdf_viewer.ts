1ffb6c349ae9725a36bd4512bd83aba0054e7c15
Jakob Pinterits <jakob.pinterits@gmail.com>
2025-03-22 19:50:32 +0100

    add PDF viewer component

---
+++ frontend/code/components/pdf_viewer.ts
@@ -0,0 +1,53 @@
+import { applyIcon } from "../designApplication";
+import { ComponentBase, ComponentState, DeltaState } from "./componentBase";
+
+export type PdfViewerState = ComponentState & {
+    _type_: "PdfViewer-builtin";
+    pdfUrl: string;
+};
+
+export class PdfViewerComponent extends ComponentBase<PdfViewerState> {
+    private objectElement: HTMLObjectElement;
+    private fallbackColumn: HTMLElement;
+
+    createElement(): HTMLElement {
+        let element = document.createElement("div");
+        element.classList.add("rio-pdf-viewer");
+
+        // Create an object element for PDF viewing
+        this.objectElement = document.createElement("object");
+        this.objectElement.type = "application/pdf";
+        this.objectElement.setAttribute("aria-label", "PDF document");
+        element.appendChild(this.objectElement);
+
+        // Add fallback content, should the object element fail to load
+        this.fallbackColumn = document.createElement("div");
+        this.fallbackColumn.classList.add("rio-pdf-viewer-fallback");
+        this.objectElement.appendChild(this.fallbackColumn);
+
+        let iconElement = document.createElement("div");
+        this.fallbackColumn.appendChild(iconElement);
+        applyIcon(iconElement, "material/error", "danger");
+
+        let textElement = document.createElement("div");
+        textElement.innerHTML = `The PDF document cannot be displayed in your browser.<br>You can try to <a href="${this.state.pdfUrl}" download target="_blank">download</a> it instead.`;
+        this.fallbackColumn.appendChild(textElement);
+
+        return element;
+    }
+
+    updateElement(
+        deltaState: DeltaState<PdfViewerState>,
+        latentComponents: Set<ComponentBase>
+    ): void {
+        super.updateElement(deltaState, latentComponents);
+
+        if (
+            deltaState.pdfUrl !== undefined &&
+            this.objectElement.data !== deltaState.pdfUrl
+        ) {
+            // Update the PDF source
+            this.objectElement.data = deltaState.pdfUrl;
+        }
+    }
+}
---
+++ rio/components/pdf_viewer.py
@@ -0,0 +1,134 @@
+from __future__ import annotations
+
+import typing as t
+from pathlib import Path
+
+from uniserde import JsonDoc
+from yarl import URL
+
+from .. import assets
+from .fundamental_component import FundamentalComponent
+
+__all__ = ["PdfViewer"]
+
+
+@t.final
+class PdfViewer(FundamentalComponent):
+    """
+    Displays a PDF document.
+
+    `PdfViewer` allows you to display PDF documents from various sources. The
+    PDF can be loaded from a URL or a local file. If the document is already
+    hosted somewhere, you can provide a `rio.URL` for easy access. To display a
+    local file use a `pathlib.Path` object. Finally, if you already have the
+    document data in memory you can pass it as a `bytes` object.
+
+    Note that unlike most components in Rio, the `PdfViewer` component does not
+    have a large `natural` size, but instead only displays part of the document
+    if allocated to little space.
+
+
+    ## Attributes
+
+    `pdf`: The PDF document to display.
+
+
+    ## Examples
+
+    This example will display a PDF document hosted on the web:
+
+    ```python
+    rio.PdfViewer(
+        rio.URL("https://example.com/document.pdf"),
+    )
+    ```
+
+    You can also display PDFs from a path:
+
+    ```python
+    from pathlib import Path
+
+    rio.PdfViewer(
+        Path("example_document.pdf"),
+    )
+    ```
+
+    You can access the `App`'s assets directory using the `assets` property.
+    This will return a `pathlib.Path` object pointing to the assets directory:
+
+    ```python
+    rio.PdfViewer(
+        Path(self.session.assets / "example_document.pdf"),
+        min_width=20,
+        min_height=20,
+    )
+    ```
+    """
+
+    pdf: Path | URL | bytes
+
+    def __init__(
+        self,
+        pdf: Path | URL | bytes,
+        *,
+        key: str | int | None = None,
+        margin: float | None = None,
+        margin_x: float | None = None,
+        margin_y: float | None = None,
+        margin_left: float | None = None,
+        margin_top: float | None = None,
+        margin_right: float | None = None,
+        margin_bottom: float | None = None,
+        min_width: float = 5,
+        min_height: float = 5,
+        # MAX-SIZE-BRANCH max_width: float | None = None,
+        # MAX-SIZE-BRANCH max_height: float | None = None,
+        grow_x: bool = False,
+        grow_y: bool = False,
+        align_x: float | None = None,
+        align_y: float | None = None,
+        # SCROLLING-REWORK scroll_x: t.Literal["never", "auto", "always"] = "never",
+        # SCROLLING-REWORK scroll_y: t.Literal["never", "auto", "always"] = "never",
+    ) -> None:
+        super().__init__(
+            key=key,
+            margin=margin,
+            margin_x=margin_x,
+            margin_y=margin_y,
+            margin_left=margin_left,
+            margin_top=margin_top,
+            margin_right=margin_right,
+            margin_bottom=margin_bottom,
+            min_width=min_width,
+            min_height=min_height,
+            # MAX-SIZE-BRANCH max_width=max_width,
+            # MAX-SIZE-BRANCH max_height=max_height,
+            grow_x=grow_x,
+            grow_y=grow_y,
+            align_x=align_x,
+            align_y=align_y,
+            # SCROLLING-REWORK scroll_x=scroll_x,
+            # SCROLLING-REWORK scroll_y=scroll_y,
+        )
+
+        self.pdf = pdf
+
+    def _get_pdf_asset(self) -> assets.Asset:
+        pdf = self.pdf
+
+        if getattr(self, "_pdf_for_cached_asset", None) != pdf:
+            self._cached_pdf_asset = assets.Asset.new(
+                pdf,
+                media_type="application/pdf",
+            )
+            self._pdf_for_cached_asset = pdf
+
+        return self._cached_pdf_asset
+
+    def _custom_serialize_(self) -> JsonDoc:
+        return {
+            "pdfUrl": self._get_pdf_asset()._serialize(self.session),
+        }
+
+
+PdfViewer._unique_id_ = "PdfViewer-builtin"
